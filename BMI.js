0ce159b6ba550d13fe83b1459486ba15e021c3f4
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-10 12:29:04 +0200

    files commited

--- BMI.js
+++ BMI.js
@@ -7,8 +7,15 @@ const btn = document.getElementById('btn')
 const display = document.getElementById('display')
 
 const BMI = (mass)/(height/100*height/100).toFixed(2)
-document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI}`
-
-
-
+if (height || mass ==="") {
+    document.getElementById('display').innerHTML = "invalid input , please enter valid credentials"  
+}else if(BMI>=18){
+    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have under weight`   
+}else if(BMI>=25){
+    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have normal weight`   
+}else if(BMI>=30){
+    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have over weight`   
+}else{
+    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You are in obesity`  
+}
 }
