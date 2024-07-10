866e925be0cb991f2506e2d1740fca8003931550
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-10 11:43:52 +0200

    first commit

--- BMI.js
+++ BMI.js
@@ -6,7 +6,7 @@ const mass = document.getElementById('mass').value
 const btn = document.getElementById('btn')
 const display = document.getElementById('display')
 
-let BMI = (mass)/(height*0.01)*(height*0.01)
+const BMI = (mass)/(height/100*height/100).toFixed(2)
 document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI}`
 
 
