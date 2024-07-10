cd8956108160447cca4f8f1257f879e36e71cade
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-07-10 12:48:20 +0200

    bmi modified

--- BMI.js
+++ BMI.js
@@ -1,21 +1,36 @@
 
 
 function calculateBodyMassIndex(){
-const height= document.getElementById('height').value
-const mass = document.getElementById('mass').value
-const btn = document.getElementById('btn')
-const display = document.getElementById('display')
+    const height= document.getElementById('height').value
+    const mass = document.getElementById('mass').value
+    const btn = document.getElementById('btn')
+    const display = document.getElementById('display')
 
-const BMI = (mass)/(height/100*height/100).toFixed(2)
-if (height || mass ==="") {
-    document.getElementById('display').innerHTML = "invalid input , please enter valid credentials"  
-}else if(BMI>=18){
-    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have under weight`   
-}else if(BMI>=25){
-    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have normal weight`   
-}else if(BMI>=30){
-    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have over weight`   
-}else{
-    document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You are in obesity`  
+  if(height===""){
+        document.getElementById('display').innerHTML = "invalid height"
+  }else if(mass===""){
+document.getElementById('display').innerHTML = "invalid mass"
+  }
+  
+  else if(height|| mass===0){
+    document.getElementById('display').innerHTML = " please check the value of your height or your mass"
+  }
+
+    
+const BMI = (mass)/(height/100*height/100).toFixed(1)
+      
+     if(BMI>=18){
+        document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have under weight`   
+    }else if(BMI>=25){
+        document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have normal weight`   
+    }else if(BMI>=30){
+        document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You have over weight`   
+    }else{
+        document.getElementById('display').innerHTML = `YOUR BMI IS ${BMI} You are in obesity`  
+    }
+   
 }
-}
+
+
+
+   
